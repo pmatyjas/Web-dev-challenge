72de7373f5dbe58b4c6e0abe452d7eb38b5faba7
pmatyjas <matyjaspaulina@gmail.com>
2018-12-06 15:48:12 +0100

    banner

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 let menu = document.querySelector('.hamburger');
+let nav = document.querySelector('.header__menu');
+console.log(nav);
+let mobileNav = nav.cloneNode(true);
+console.log(mobileNav);
+mobileNav.classList.remove('header__menu');
+mobileNav.classList.add('header__menu-moblie');
+document.body.appendChild(mobileNav);
+
+
 
 menu.addEventListener('click', function() {
     this.classList.toggle('hamburger--active');
+    mobileNav.classList.toggle('header__mobile-nav-show')
 });
